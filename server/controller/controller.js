a35f58716f8d9182dd12937cecddd50e94d87d8a
00011347 <amukhammadiev@students.wiut.uz>
2022-04-04 01:35:25 +0500

    setting get method

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -30,7 +30,15 @@ exports.create = (req, res) => {
 };
 
 // retrieve and return all users/ retrive and return a single user
-exports.find = (req, res) => {};
+exports.find = (req, res) => {
+  Userdb.find()
+  .then(user=>{
+    res.send(user)
+  })
+  .catch(err=>{
+    res.status(500).send({message:err.message || "Error has occured while recapturing user information"})
+  })
+};
 
 // Update a new identified user by user id
 exports.update = (req, res) => {};
