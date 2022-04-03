81a3ef0f30b6fd454e48c7d20cb07e4de9aebd70
00011347 <amukhammadiev@students.wiut.uz>
2022-04-04 01:29:36 +0500

    setting creation controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,33 @@
 var Userdb = require("../model/model");
 
 // creating and saving new and
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  // validating requests
+  if (!req.body) {
+    res.status(400).send({ message: "Content cannot be empty!" });
+    return;
+  }
+
+  // new user
+  const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status
+  });
+
+  // save user in the database
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error has occured while creating operation",
+      });
+    });
+};
 
 // retrieve and return all users/ retrive and return a single user
 exports.find = (req, res) => {};
