799b0ffaca14a34f13f566f04a9c5f67bc2fcf7a
00011347 <amukhammadiev@students.wiut.uz>
2022-04-04 01:48:30 +0500

    setting update method

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -13,7 +13,7 @@ exports.create = (req, res) => {
     name: req.body.name,
     email: req.body.email,
     gender: req.body.gender,
-    status: req.body.status
+    status: req.body.status,
   });
 
   // save user in the database
@@ -32,16 +32,41 @@ exports.create = (req, res) => {
 // retrieve and return all users/ retrive and return a single user
 exports.find = (req, res) => {
   Userdb.find()
-  .then(user=>{
-    res.send(user)
-  })
-  .catch(err=>{
-    res.status(500).send({message:err.message || "Error has occured while recapturing user information"})
-  })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({
+          message:
+            err.message ||
+            "Error has occured while recapturing user information",
+        });
+    });
 };
 
 // Update a new identified user by user id
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  if(!req.body){
+    return res
+      .status(400)
+      .send({message:"Data to update cannot be empty"})
+  }
+
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body,{useFindAndModify:false})
+  .then(data=>{
+    if(!data){
+      res.status(404).send({message:`Cannot Update user with ${id}. User might not be found`})
+    }else{
+      res.send(data)
+    }
+  })
+  .catch(err=>{
+    res.status(500).send({message:"Error update user information"})
+  })
+};
 
 // Delete a user with specified user id in the request
 exports.delete = (req, res) => {};
