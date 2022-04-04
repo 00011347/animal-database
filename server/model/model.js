8e5ae2268847c8aaac791c7f45bdb1a80eb5eb62
00011347 <amukhammadiev@students.wiut.uz>
2022-04-04 18:32:02 +0500

    adding carousel

--- server/model/model.js
+++ server/model/model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 var schema = new mongoose.Schema({
   name: { type: String, required: true },
-  email:{ type:String, required:true, unique:true},
+  email:{ type:String, required:true},
   gender: String,
   status: String
 });
